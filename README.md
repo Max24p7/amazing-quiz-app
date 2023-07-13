3f998bbe6e884e9ba1f8150576299a0e606fb305
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-07-13 14:59:04 -0500

    changed background, added functioning timer and score keeping

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 https://www.youtube.com/watch?v=riDzcEQbX6k
 
 https://unused-css.com/blog/css-outer-glow/
+
+https://cssgradient.io/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const saveButton = document.getElementById('save-btn')
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
+const inputElement = document.getElementById('input');
+const timerElement = document.getElementById('timer');
+const messageElement = document.getElementById('message')
 
 let shuffledQuestions, currentQuestionIndex;
+
+let timer = 60; //set timer to 60 seconds
+
+function updateTimer(){ // lower timer by 1 each second
+    timer--
+    timerElement.textContent = timer;
+    if (timer === 0) {
+        clearInterval(timerInterval); endGame(); //if timer reaches 0, run endGame function
+    }
+}
+
+let timerInterval;
 
 startButton.addEventListener('click', startGame) //creates clickable start button function
 nextButton.addEventListener('click', () => {
@@ -19,6 +35,9 @@ function startGame() {
     currentQuestionIndex = 0  
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
+    timer = 60;
+    timerElement.textContent = timer;
+    timerInterval = setInterval(updateTimer, 1000);
 }
 
 function setNextQuestion() { //displays the shuffled questions that are arrayed below
@@ -30,10 +49,10 @@ function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
         const button = document.createElement('button'); //creates a button that features the answers created below
-        button.innerText = answer.text
+        button.innerText = answer.text  //adds 'answer' arrays into the buttons
         button.classList.add('btn')
         if (answer.correct) {
-            button.dataset.correct = answer.correct
+            button.dataset.correct = answer.correct //if answer is correct, adds correct styling. otherwise adds wrong styling
         }
         button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
@@ -54,12 +73,52 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {  //if user has gone through all of the questions available, the 'start' button will appear as a 'reset' button, allowing the user to restart the game.
     nextButton.classList.remove('hide')
     } else {
+        clearInterval(timerInterval); //end the game and stop the timer if the user runs out of questions
+        endGame();
         startButton.innerText = 'Reset'
         startButton.classList.remove('hide')
     }
+    if (!correct) {
+        timer -= 10; //If question is incorrect, remove 10 seconds from timer.
+        if (timer < 0) { //If timer goes below zero, set timer to zero, ending the game.
+            timer = 0;
+        }
+        timerElement.textContent = timer;
+    }
+}
+
+function endGame() { //function that ends game and displays final score
+    questionContainerElement.classList.add('hide')
+    messageElement.textContent = `Final Score: ${timer}` //displays the final score at the top
+    messageElement.classList.remove('hide')
+    inputElement.classList.remove('hide')
+    saveButton.classList.remove('hide')
+    saveButton.addEventListener('click', saveScore); //makes the initial input and save button appear
+    startButton.innerText = "Reset"; //makes the reset button appear when the timer reaches zero
+    startButton.classList.remove('hide');
+}
+
+function saveScore() { //function that saves scores to local storage (sometimes?)
+    const initials = inputElement.value 
+    let scores = localStorage.getItem('scores')//retrieve the value of the final score
+    if (scores) {
+        scores = JSON.parse(scores)
+    } else {
+        scores = [];
+    }
+    const score = {
+        initials: initials,
+        score: timer,
+    }
+    scores.push(score);
+    localStorage.setItem('scores', JSON.stringify(scores));
+    inputElement.classList.add('hide'); //Hides the initials input and save button after saving the score
+    saveButton.classList.add('hide');
+    console.log(`Initials: ${score.initials}`); //final scores will be logged to the console
+    console.log(`Final score: ${score.score}`);
 }
 
 function setStatusClass(element, correct) { 
@@ -77,11 +136,11 @@ function clearStatusClass(element) {
 }
 
 
-const questions = [
+const questions = [ //Arrays of questions and answers to be pulled for the shuffler
     {
         question: 'Which of the following answers will create a line break?',
         answers: [
-            {text: '<br>', correct: true },
+            {text: '<br>', correct: true }, //'correct: true' indicates that that is the correct answer
             {text: '<gap>', correct: false },
             {text: '<div>', correct: false },
             {text: '<h1>', correct: false },
@@ -121,6 +180,42 @@ const questions = [
             {text: 'Hot Dogs', correct: false },
             {text: 'Ice Cream', correct: false },
             {text: 'Hawaiian Pizza', correct: true },
+        ]
+    },
+    {
+        question: 'Which of the following is not a Primitive data type?',
+        answers: [
+            {text: 'string', correct: false },
+            {text: 'boolean', correct: false },
+            {text: 'planet', correct: true },
+            {text: 'null', correct: false },
+        ]
+    },
+    {
+        question: 'Which of the following is not a font sizing option?',
+        answers: [
+            {text: 'px', correct: true },
+            {text: 'fraction', correct: false },
+            {text: 'rem', correct: false },
+            {text: '%', correct: false },
+        ]
+    },
+    {
+        question: 'How should you declare a variable that can be changed later?',
+        answers: [
+            {text: 'const', correct: false },
+            {text: 'move', correct: false },
+            {text: 'turn', correct: false },
+            {text: 'let', correct: true },
+        ]
+    },
+    {
+        question: 'How much time does this quiz start with?',
+        answers: [
+            {text: '60 seconds', correct: true },
+            {text: '20 seconds', correct: false },
+            {text: '40 seconds', correct: false },
+            {text: '120 seconds', correct: false },
         ]
     },
 
